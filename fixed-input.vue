0e7988206e8ea9dd12ab978474efd8286ccfe1be
wangweiwei <wangweiwei@daqinjia.cn>
2020-06-19 11:43:45 +0800

    feat: ios失焦回弹, 去除ios阴影，去除outline

---
+++ fixed-input.vue
@@ -0,0 +1,28 @@
+<template>
+  <input type="text" @blur="blur">
+</template>
+<script>
+export default {
+  data() {
+    return {
+      windowHeight: window.innerHeight
+    }
+  },
+  methods: {
+    blur() {
+      if (this.windowHeight == window.innerHeight) {
+        return
+      }
+      window.scrollTo(0)
+    }
+  }
+}
+</script>
+<style scoped>
+input {
+  -webkit-appearance: none;  
+  outline: none
+}
+</style>
+
+
